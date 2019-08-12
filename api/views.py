2aef890e12d2405da8866f362376df917e67b6ff
Allan Gonçalves <allangoncalves@outlook.com>
2019-08-12 10:02:48 -0300

    view working with nested router

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from api.serializers import *
 
 # Create your views here.
+
+class SpotViewSet(viewsets.ModelViewSet):
+    serializer_class = SpotSerializer
+
+    def get_queryset(self):
+        return Spot.objects.filter(city=self.kwargs['city_pk'])
+
+class CityViewSet(viewsets.ModelViewSet):
+    serializer_class = CitySerializer
+
+    def get_queryset(self):
+        return City.objects.filter(state=self.kwargs['state_pk'])
+
+class StateViewSet(viewsets.ModelViewSet):
+    serializer_class = StateSerializer
+    queryset = State.objects.all()
