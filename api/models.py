551be2394ef6dc347f7f4f880e42c37164cd036e
Allan Gonçalves <allangoncalves@outlook.com>
2019-08-12 10:01:54 -0300

    state, city and spot relations

--- api/models.py
+++ api/models.py
@@ -23,12 +23,55 @@ WAVE_STRENGTH = (
     ('soft', 'Suave'),
 )
 
+class AccessPoint(models.Model):
+    stairwell = models.BooleanField(default=False)
+    cliff = models.BooleanField(default=False)
+    bay = models.BooleanField(default=False)
+    trail = models.BooleanField(default=False)
+    other = models.CharField(max_length=220, default="", blank=True)
+
+class GroundPoint(models.Model):
+    rock = models.BooleanField(default=False)
+    sand = models.BooleanField(default=False)
+    coral = models.BooleanField(default=False)
+
+class BestTideMoves(models.Model):
+    low = models.BooleanField(default=False)
+    high = models.BooleanField(default=False)
+    ebb = models.BooleanField(default=False)
+    flood = models.BooleanField(default=False)
+
+class WaveDirection(models.Model):
+    right = models.BooleanField(default=False)
+    left = models.BooleanField(default=False)
+
+class SurfLevelNeeded(models.Model):
+    beginner = models.BooleanField(default=False)
+    intermediate = models.BooleanField(default=False)
+    expert = models.BooleanField(default=False)
+
+class WaveFrequency(models.Model):
+    low = models.BooleanField(default=False)
+    regular = models.BooleanField(default=False)
+    high = models.BooleanField(default=False)
+
+class State(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class City(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    state = models.ForeignKey(State, related_name="cities", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Spot(models.Model):
     name = models.CharField(max_length=100, blank=False)
-    city = models.CharField(max_length=100, blank=False)
-    state = models.CharField(max_length=100, blank=False)
     car = models.BooleanField(default=False)
+    city = models.ForeignKey(City, related_name="spots", on_delete=models.CASCADE)
     special_access = models.BooleanField(default=False)
     beach_type = models.CharField(max_length=100, choices=BEACH_TYPE)
     access_comment = models.TextField()
@@ -38,42 +81,14 @@ class Spot(models.Model):
     weekend_crowd = models.CharField(max_length=50, blank=False, choices=CROWDS)
     wave_quality = models.IntegerField()
     wave_length = models.CharField(max_length=10, blank=False, choices=WAVE_LENGTHS)
-    strength = models.CharField(max_length=10, blank=False, choices=WAVE_STRENGTH)
+    wave_strength = models.CharField(max_length=10, blank=False, choices=WAVE_STRENGTH)
+    accesses = models.OneToOneField(AccessPoint, on_delete=models.CASCADE)
+    grounds = models.OneToOneField(GroundPoint, on_delete=models.CASCADE)
+    best_tide_moves = models.OneToOneField(BestTideMoves, on_delete=models.CASCADE)
+    wave_directions = models.OneToOneField(WaveDirection, on_delete=models.CASCADE)
+    surf_levels = models.OneToOneField(SurfLevelNeeded, on_delete=models.CASCADE)
+    wave_frequencies = models.OneToOneField(WaveFrequency, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-class AccessPoint(models.Model):
-    stairwell = models.BooleanField(default=False)
-    cliff = models.BooleanField(default=False)
-    bay = models.BooleanField(default=False)
-    trail = models.BooleanField(default=False)
-    other = models.CharField(max_length=220, default="", blank=True)
-    spot = models.ForeignKey(Spot, related_name='accesses', on_delete=models.CASCADE)
-
-class GroundPoint(models.Model):
-    rock = models.BooleanField(default=False)
-    sand = models.BooleanField(default=False)
-    coral = models.BooleanField(default=False)
-    spot = models.ForeignKey(Spot, related_name='grounds', on_delete=models.CASCADE)
-
-class BestTideMoves(models.Model):
-    low = models.BooleanField(default=False)
-    high = models.BooleanField(default=False)
-    ebb = models.BooleanField(default=False)
-    flood = models.BooleanField(default=False)
-    spot = models.ForeignKey(Spot, related_name='best_tide_moves', on_delete=models.CASCADE)
-
-class WaveDirection(models.Model):
-    right = models.BooleanField(default=False)
-    left = models.BooleanField(default=False)
-    spot = models.ForeignKey(Spot, related_name='wave_directions', on_delete=models.CASCADE)
-
-class SurfLevelNeeded(models.Model):
-    beginner = models.BooleanField(default=False)
-    intermediate = models.BooleanField(default=False)
-    expert = models.BooleanField(default=False)
-    spot = models.ForeignKey(Spot, related_name='surf_levels', on_delete=models.CASCADE)
-
-class WaveFrequency(models.Model):
-    low = models.BooleanField(default=False)
-    regular = models.BooleanField(default=False)
-    high = models.BooleanField(default=False)
-    spot = models.ForeignKey(Spot, related_name='wave_frequencies', on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.name}'
