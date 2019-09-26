ad1a163cde10089daeea62907eafa7a06d00208b
Allan Gonçalves <allangoncalves@outlook.com>
2019-09-26 16:08:28 -0300

    update requirements and config django-heroku

--- dicasurf/settings.py
+++ dicasurf/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import datetime
+import django_heroku
+django_heroku.settings(locals(), staticfiles=False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
