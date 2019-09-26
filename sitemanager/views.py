0c25987acd003e1b74a79fd9ceca77f3c8aa414a
Allan Gonçalves <allangoncalves@outlook.com>
2019-09-26 19:52:12 -0300

    set homepage permissions

--- sitemanager/views.py
+++ sitemanager/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from sitemanager.serializers import *
+from rest_framework.permissions import AllowAny
+from api.permissions import IsLoggedInUserOrAdmin, IsAdminUser
 from rest_framework import viewsets
 
 # Create your views here.
 class HomePageViewSet(viewsets.ModelViewSet):
     serializer_class = HomePageSerializer
     queryset = HomePage.objects.all()
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'retrieve' or self.action == 'list':
+            permission_classes = [AllowAny]
+        elif self.action == 'create' or self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
